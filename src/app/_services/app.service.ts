b1c4c79da29bf6306c0bbe09b039f5308cb390ee
luca claessens <luca.claessens@zzapps.nl>
2019-12-27 15:09:53 +0100

    Start - Floorplan navigation

--- src/app/_services/app.service.ts
+++ src/app/_services/app.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,15 @@ export class AppService {
   private openFloorplanSource = new Subject();
   openFloorplan$ = this.openFloorplanSource.asObservable();
 
-  constructor() { }
+  activePageUrl$ = this.router.events.pipe(
+    filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+    map((e) => e.url)
+  );
+
+  // tslint:disable-next-line: no-bitwise
+  hashCode = s => s.split('').reduce((a, b) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a; }, 0);
+
+  constructor(private router: Router) { }
 
 
   requestOpenFloorplan() {
--- src/app/_shared/floorplan/floorplan.component.ts
+++ src/app/_shared/floorplan/floorplan.component.ts
@@ -1,17 +1,72 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { AppService } from 'src/app/_services/app.service';
+import { tap, map, debounceTime } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+enum routeIDToClass {
+  'L2VudHJhbmNl' = 'entrance',
+  'L2V4aGliaXRpb24veThhOGFxSjJFeE11SG1XcmVaWkk/ZWlkeD0w' = 'sevenclicks',
+  'L2V4aGliaXRpb24veThhOGFxSjJFeE11SG1XcmVaWkk/ZWlkeD0x' = 'amphora'
+}
 
 @Component({
   selector: 'museum-floorplan',
   templateUrl: './floorplan.component.html',
   styleUrls: ['./floorplan.component.scss']
 })
-export class FloorplanComponent implements OnInit {
+export class FloorplanComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('pointer', { static: true }) pointer;
 
-  @Input() routeId = 'entrance';
+  activeRouteAsClass$ = this.app.activePageUrl$.pipe(
+    map((url) => {
+      const hash = btoa(url);
+      return hash;
+    }),
+    tap(console.warn),
+    map((url) => routeIDToClass[url])
+  );
 
-  ngOnInit() {
+  svgClicked(event: MouseEvent) {
+    const tappedElem = event.target as HTMLElement;
+
+    // Did we click a tappable rectangle?
+    if (tappedElem.nodeName === 'rect') {
+      const nearestHoverableParent = tappedElem.parentElement;
+      // is it in the right type of container?
+      if (nearestHoverableParent.nodeName === 'g'
+        && nearestHoverableParent.classList.contains('hoverable')) {
+        const target = nearestHoverableParent.id;
+        const matchingMapEntry = Object.entries(routeIDToClass).find(([_k, v]) => v === target);
+        // If we find a configured route to navigate to, we go there.
+        if (matchingMapEntry) {
+          const routeOfTarget = atob(matchingMapEntry[0]);
+          this.router.navigateByUrl(routeOfTarget);
+        }
+      }
+    }
+
+  }
+
+  loadPointerBeneath(node: HTMLElement) {
+    const pointer = this.pointer.nativeElement;
+    const top = node.offsetTop;
+    const left = node.offsetLeft;
+
+    pointer.style.top = `${top} px`;
+    pointer.style.left = `${left} px`;
+  }
+
+  constructor(private app: AppService, private router: Router, private elRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.activeRouteAsClass$.pipe(
+      debounceTime(500),
+      map((className) => {
+        const node = this.elRef.nativeElement.querySelector(`#${className}-text`);
+        return node;
+      })
+    ).subscribe(node => this.loadPointerBeneath(node));
   }
 
 }
