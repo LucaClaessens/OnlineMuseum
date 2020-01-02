974cb7232f708c2f8b57b52038fc661d2f387af3
luca claessens <luca.claessens@zzapps.nl>
2020-01-02 18:46:41 +0100

    Added dynamic footer text

--- src/app/exhibition/exhibition-content/amphora-page/amphora-page.component.ts
+++ src/app/exhibition/exhibition-content/amphora-page/amphora-page.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'museum-amphora-page',
   templateUrl: './amphora-page.component.html',
   styleUrls: ['./amphora-page.component.scss']
 })
-export class AmphoraPageComponent implements OnInit {
+export class AmphoraPageComponent implements AfterViewInit {
 
   timeIndicators: number[];
+  footerText = 'Loading..';
+  private observer: IntersectionObserver;
+  private footerTextEntries: { year: number, text: string }[] = [
+    { year: -530, text: 'Initial value loaded from array (-530)' },
+    { year: -520, text: 'it is def -520😎' },
+    { year: -510, text: '-510 ⏰' },
+    { year: -490, text: 'Vases are objects! (-490)' },
+    { year: -480, text: `Haha now it's 480 BC right?` }
+  ];
+
+  @ViewChild('drag', { read: ElementRef, static: true }) drag: ElementRef;
 
   constructIndicators(yearStart: number, yearEnd: number) {
 
@@ -21,12 +32,40 @@ export class AmphoraPageComponent implements OnInit {
     return indicators;
   }
 
-  constructor() {
+  loadFooterTextForYearId(yearId: number) {
+    const footerTextEntry = this.footerTextEntries.find(e => e.year === yearId);
+    if (footerTextEntry) {
+      this.footerText = footerTextEntry.text;
+    }
+  }
+
+  constructObserver(nodes: HTMLElement[]) {
+
+    const opts: IntersectionObserverInit = {
+      threshold: 0.5
+    };
+
+    const callback = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      const visible = entries.filter(e => e.isIntersecting);
+      visible.forEach(entry => {
+        const dataset = (entry.target as HTMLDivElement).dataset;
+        const yearIdAsNumber = Number(dataset.yearId);
+        this.loadFooterTextForYearId(yearIdAsNumber);
+      });
+    };
+
+    this.observer = new IntersectionObserver(callback, opts);
+
+    nodes.forEach(node => this.observer.observe(node));
+  }
+
+  constructor(private elRef: ElementRef) {
     this.timeIndicators = this.constructIndicators(-530, 230);
   }
 
-  ngOnInit() {
-
+  ngAfterViewInit() {
+    const indicators: HTMLDivElement[] = Array.from(this.drag.nativeElement.querySelectorAll('div.time-indicator'));
+    this.constructObserver(indicators);
   }
 
 }
